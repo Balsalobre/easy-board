67d9c95d03078b22f3b4e80694c0728fff8b2600
balsalobre <carlosocb@gmail.com>
2025-03-11 23:21:55 +0100

    enhance whiteboard features with font size, weight, and color controls; improve styling and layout

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,42 @@
 import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<div id="controls">
-<label for="fontSize">Tamaño:</label>
-<select id="fontSize">
-  <option value="1.5rem">Pequeño</option>
-  <option value="2rem">Mediano</option>
-  <option value="3rem" selected>Muy Grande</option>
-</select>
+  <div id="board-container">
+    <div id="controls">
+      <label for="fontSize">Tamaño:</label>
+      <select id="fontSize">
+        <option value="3rem">Pequeño</option>
+        <option value="6rem" selected>Mediano</option>
+        <option value="9rem">Grande</option>
+      </select>
 
-<label for="fontWeight">Grosor:</label>
-<select id="fontWeight">
-  <option value="400">Normal</option>
-  <option value="700" selected>Negrita</option>
-</select>
-</div>
-<div id="board" contenteditable="true">
-¡Escribe en la pizarra!
-</div>
-
+      <label for="fontWeight">Grosor:</label>
+      <select id="fontWeight">
+        <option value="400">Normal</option>
+        <option value="700" selected>Negrita</option>
+      </select>
+      
+      <label for="chalkColor">Color:</label>
+      <select id="chalkColor">
+        <option value="rgba(255, 255, 255, 0.9)" selected>Blanco</option>
+        <option value="rgba(255, 235, 59, 0.9)">Amarillo</option>
+        <option value="rgba(255, 171, 145, 0.9)">Rosa</option>
+        <option value="rgba(129, 212, 250, 0.9)">Azul</option>
+      </select>
+    </div>
+    <div id="board" contenteditable="true">
+      ¡Escribe en la pizarra!
+    </div>
+  </div>
 `;
 
 const fontSizeSelect = document.getElementById('fontSize') as HTMLSelectElement;
-const fontWeightSelect = document.getElementById('fontWeight') as HTMLSelectElement;
+const fontWeightSelect = document.getElementById(
+  'fontWeight'
+) as HTMLSelectElement;
+const chalkColorSelect = document.getElementById(
+  'chalkColor'
+) as HTMLSelectElement;
 const board = document.getElementById('board') as HTMLDivElement;
 
 fontSizeSelect.addEventListener('change', () => {
@@ -31,4 +45,12 @@ fontSizeSelect.addEventListener('change', () => {
 
 fontWeightSelect.addEventListener('change', () => {
   board.style.fontWeight = fontWeightSelect.value;
+});
+
+chalkColorSelect.addEventListener('change', () => {
+  board.style.color = chalkColorSelect.value;
+
+  const color = chalkColorSelect.value.replace('0.9', '0.6');
+  const color2 = chalkColorSelect.value.replace('0.9', '0.3');
+  board.style.textShadow = `0 0 2px ${color}, 0 0 4px ${color2}`;
 });
